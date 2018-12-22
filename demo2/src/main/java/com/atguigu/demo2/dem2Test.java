c23b9f52f145479af0e77a05bc5ba7f7cd6f4337
tom <tom@atguigu.com>
2018-12-22 12:59:34 +0800

    gmallTest已提交

---
+++ demo2/src/main/java/com/atguigu/demo2/dem2Test.java
@@ -0,0 +1,19 @@
+package com.atguigu.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class dem2Test {
+
+    @Controller
+    public class IndexController {
+
+        @RequestMapping("/index")
+        @ResponseBody
+        public String Index(){
+            return "index";
+        }
+
+    }
+}
