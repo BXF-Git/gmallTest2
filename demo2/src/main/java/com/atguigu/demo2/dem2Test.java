4ea67981897b6d934b32fbd624743dc18ca797ab
tom <tom@atguigu.com>
2018-12-24 11:05:15 +0800

    gmallTest这哪

--- demo2/src/main/java/com/atguigu/demo2/dem2Test.java
+++ demo2/src/main/java/com/atguigu/demo2/dem2Test.java
@@ -19,7 +19,7 @@ public class dem2Test {
         @RequestMapping("/index")
         @ResponseBody
         public String Index(){
-            return "你是猪吗";
+            return "你是谁啊，你在哈";
         }
 
 
